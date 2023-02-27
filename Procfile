3d5196ee46e1c032ab858b38da8868db75ee2e4f
Alex Mack <olegbrain0@gmail.com>
2023-02-28 04:26:26 +0500

    created procfile, edited settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn restaurant.wsgi --log-file -
--- restaurant/settings/production.py
+++ restaurant/settings/production.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+DB_NAME = os.environ.get('DB_NAME')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_USER = os.environ.get('DB_USER')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
