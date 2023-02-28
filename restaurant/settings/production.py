a2f7609777487f996ef1bc4497d8a9eb323159ca
Alex Mack <olegbrain0@gmail.com>
2023-02-28 07:53:42 +0500

    Make work it

--- restaurant/settings/production.py
+++ restaurant/settings/production.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -20,13 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--z&*pz-w)6i@1cj+06!&hln_n(tx@66ub&s9fnvq+_e(oi&hrj'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-9f65.up.railway.app/'
+]
 
 # Application definition
 
