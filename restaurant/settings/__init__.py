ba13eeaa09f2d514012bdb806b3c3640c0075ae6
Alex Mack <olegbrain0@gmail.com>
2023-02-28 07:35:22 +0500

    Work

--- restaurant/settings/__init__.py
+++ restaurant/settings/__init__.py
@@ -1,5 +1,5 @@
 from .production import *
 try:
-    from .production import *
+    from .local_settings import *
 except ImportError:
     print('No local_settings.py was found. You must be on production.')
