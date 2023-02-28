807223710745c19e581d80c1f4d3ce3c69f35ff7
Alex Mack <olegbrain0@gmail.com>
2023-02-28 06:47:15 +0500

    Commit just this

--- restaurant/settings/production.py
+++ restaurant/settings/production.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure--z&*pz-w)6i@1cj+06!&hln_n(tx@66ub&s9fnvq+_e(oi&hrj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
