7c02526eca987ecbaabf94ce0a4a334dbfdd93a4
Alex Mack <olegbrain0@gmail.com>
2023-02-28 07:33:43 +0500

    Make work

--- restaurant/settings/__init__.py
+++ restaurant/settings/__init__.py
@@ -1,5 +1,5 @@
 from .production import *
 try:
-    from .local_settings import *
+    from .production import *
 except ImportError:
     print('No local_settings.py was found. You must be on production.')
--- restaurant/settings/production.py
+++ restaurant/settings/production.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure--z&*pz-w)6i@1cj+06!&hln_n(tx@66ub&s9fnvq+_e(oi&hrj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
